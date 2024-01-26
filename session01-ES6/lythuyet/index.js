8788b1caae985c8d1e05170b87e0d44db0a31afd
phankhacdientrang <Trang93190@donga.edu.vn>
2024-01-26 18:40:30 +0700

    up

---
+++ session01-ES6/lythuyet/index.js
@@ -0,0 +1,6 @@
+import sayHello from "./export.js";
+// console.log("1111111",sayHello());
+// import {b,fullName} from "./export.js";
+// console.log("1111", fullName);
+import { fullName as name } from "./export.js";
+console.log("1111",name);
